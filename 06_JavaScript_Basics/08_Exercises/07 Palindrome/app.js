b14cd772967be9aa699b1d8f192beade87fa0489
Alin Grigorescu <alin.grig87@gmail.com>
2023-10-09 20:54:41 +0300

    Palindrome starter code - part 3

--- 06_JavaScript_Basics/08_Exercises/07 Palindrome/app.js
+++ 06_JavaScript_Basics/08_Exercises/07 Palindrome/app.js
@@ -2,5 +2,15 @@ var strInput = document.getElementById('string');
 
 function checkPalindrome() {
 	var str = strInput.value;
-	console.log(str);
+	var i = 0;
+	var j = str.length - 1;
+	while(i < j) {
+		if(cazul negativ) {
+			console.log("nu e palindrom");
+			return;
+		}
+		i++;
+		j--;
+	}
+	console.log("e palindrom");
 }
