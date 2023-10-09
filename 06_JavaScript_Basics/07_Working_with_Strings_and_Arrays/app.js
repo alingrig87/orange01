988b41d3abed3e0ccbdffaf765b3d70e9f06bab5
Alin Grigorescu <alin.grig87@gmail.com>
2023-10-09 20:09:14 +0300

    Palindrome starter code

--- 06_JavaScript_Basics/07_Working_with_Strings_and_Arrays/app.js
+++ 06_JavaScript_Basics/07_Working_with_Strings_and_Arrays/app.js
@@ -28,3 +28,54 @@ for (var i = 0; i <= arr.length - 1; i++) {
 console.log(arrOfIdxForMaxValue);
 
 console.log(max);
+
+var arr1 = ['A', 'n', 'a', ' ', 'a', 'r', 'e', ' ', 'm', 'e', 'r', 'e', '.'];
+console.log(arr1[0]);
+console.log(arr1[arr1.length - 1]);
+
+var str = 'Ana are mere.';
+console.log(str[0]);
+console.log(str[str.length - 2]);
+
+var words = str.split(' ');
+console.log(words);
+
+var dateArr = ['12', '02', '2023'];
+var dateStr = dateArr.join('-');
+console.log(dateStr);
+
+var a = 'Ana';
+var b = 'are';
+var c = 'mere';
+var concatStr = a + ' ' + b + ' ' + c + '.';
+// var concatStr = a.concat(' ').concat(b).concat(' ').concat(c).concat('.');
+console.log(concatStr);
+
+var name = 'Costel';
+var age = 35;
+
+console.log('Ma numesc ' + name + ' si am ' + (age - 1) + ' ani.');
+console.log(age + 1 + ' ani' + ' am implinit');
+console.log(22 + 1);
+console.log('22' + 1);
+console.log(22 + 1 + '1' + 22 + 1);
+console.log('Ma numesc Costel si am ' + 35 * 2);
+
+console.log('Ana are mere'.indexOf('aree'));
+
+var cnp = '1921212232434';
+var cnpArr = cnp.split('');
+console.log(cnpArr);
+cnpArr[0] = '2';
+cnp[0] = '2';
+cnp = '2' + cnp.substring(1);
+console.log(cnp);
+console.log(cnpArr);
+
+console.log(cnp.substring(1, 3));
+console.log(cnp.slice(1, 3));
+
+console.log('Ana are mere'.toLowerCase());
+console.log('Ana are mere'.toUpperCase());
+
+console.log('    Ana are mere    '.trim());
