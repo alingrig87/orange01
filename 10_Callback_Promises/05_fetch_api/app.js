62c70687c4277b158a50db9b20cdbbd6f3b3843c
Alin Grigorescu <alin.grig87@gmail.com>
2023-11-08 20:50:58 +0200

    Fetch API

--- 10_Callback_Promises/05_fetch_api/app.js
+++ 10_Callback_Promises/05_fetch_api/app.js
@@ -20,3 +20,63 @@ function showProductsCards() {
 					.join(''))
 		);
 }
+
+const postBtn = document.getElementById('post');
+postBtn.addEventListener('click', addNewProduct);
+
+function addNewProduct() {
+	const url = 'https://654bc6c15b38a59f28efb3a2.mockapi.io/products';
+
+	const product = {
+		name: 'ACG Alin',
+		imageURL:
+			'https://auchan.vtexassets.com/arquivos/ids/164754-1200-auto?v=637985891528600000&width=1200&height=auto&aspect=true',
+		price: 3.35,
+		description: 'Apa carbogazoasa minerala naturala Tusnad, 2l',
+	};
+
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(product),
+	};
+
+	fetch(url, options).then((response) => console.log(response));
+}
+
+const putBtn = document.getElementById('put');
+putBtn.addEventListener('click', updateImg);
+
+function updateImg() {
+	const url = 'https://654bc6c15b38a59f28efb3a2.mockapi.io/products/1';
+
+	const product = {
+		imageURL:
+			'https://www.apple.com/newsroom/images/product/iphone/geo/Apple_iPhone-13-Pro_iPhone-13-Pro-Max_GEO_09142021_inline.jpg.slideshow-large_2x.jpg',
+	};
+
+	const options = {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(product),
+	};
+
+	fetch(url, options).then((response) => console.log(response));
+}
+
+const deleteBtn = document.getElementById('delete');
+deleteBtn.addEventListener('click', deleteProduct);
+
+function deleteProduct() {
+	const url = 'https://654bc6c15b38a59f28efb3a2.mockapi.io/products/20';
+
+	const options = {
+		method: 'DELETE',
+	};
+
+	fetch(url, options).then((response) => console.log(response));
+}
